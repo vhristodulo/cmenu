9f9da47967cdb6ef38347c5b1910e19260bbda74
Vladislav Hristodulo <vladislav.hristodulo@gmail.com>
2015-02-11 00:36:31 +0100

    Create cmenu.php

---
+++ application/views/cmenu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>cmenu</title>
+		<link rel="stylesheet" type="text/css" href="assets/cmenu.css"/>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script type="text/javascript" src="assets/cmenu.js"></script>
+	</head>
+	<body>
+		<div id="cmenu">
+			{cmenu}
+		</div>
+	</body>
+</html>
