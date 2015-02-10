801562f04d010caf35db02c16ec58b3275505098
Vladislav Hristodulo <vladislav.hristodulo@gmail.com>
2015-02-11 00:34:08 +0100

    Create cmenu.php

---
+++ application/config/cmenu.php
@@ -0,0 +1,9 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['table_name'] = '';
+$config['pk_column_name'] = '';
+$config['fk_column_name'] = '';
+$config['title_column_name'] = '';
+
+/* End of file cmenu.php */
+/* Location: ./application/config/cmenu.php */
